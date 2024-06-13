6525e7e34580a7d91da049cae7f90eaae9f1c9f8
Tatyana <taf2002@yandex.ru>
2024-06-13 15:37:06 +0300

    logging implementation initiated

--- logging.sh
+++ logging.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-# to implement
+# install lowatch
+sudo apt update
+sudo apt install -y logwatch mailutils
+
+# ! ATTENTION !
+# !!! TO GOOGLE AND REWRITE LOGWATCH COMMAND !!!
+# prepare email template and logwatch command, save as script
+cat <<EOF >$HOME/logwatch.sh
+logwatch --detail Med --mailto root --service all --range today
+EOF
+sudo mkdir -p /archive
+sudo mv $HOME/logwatch.sh /archive/
+sudo chmod +x /archive/logwatch.sh
+
+# schedule chron job
+if ! sudo crontab -l | grep -q '0 8 \* \* \* /archive/logwatch.sh'; then 
+	sudo crontab -l | {                                                  
+		cat                                                          
+		echo '0 8 * * * /archive/logwatch.sh'                   
+	} | sudo crontab -                                                   
+fi                                                                           
+
+# force run script for screenshot
+/archive/logwatch.sh
+
+# check mail for screenshot
+# sudo mail
